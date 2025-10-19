53c8162bdd7c0a08f8b3c31862bbcf4eae79e45d
Christopher Jonota <cristopherjonota@gmail.com>
2025-10-19 19:40:16 +0800

    update datasource

--- app/src/main/java/com/example/rs_link/data/local/UserPrefsDataSource.kt
+++ app/src/main/java/com/example/rs_link/data/local/UserPrefsDataSource.kt
@@ -1,6 +1,21 @@
 package com.example.rs_link.data.local
 
 import android.preference.Preference
+import kotlinx.coroutines.flow.Flow
 import javax.inject.Inject
 
-interface UserPrefsDataSource
+interface UserPrefsDataSource{
+
+    // onboarding status
+    fun hasSeenOnboarding(): Flow<Boolean>
+    suspend fun setOnboardingComplete(isComplete: Boolean)
+
+    // Theme mode
+    fun getThemeMode(): Flow<String>
+    suspend fun setThemeMode(mode: String)
+
+    // Log In
+    fun isLoggedin(): Flow<Boolean>
+    suspend fun setLoggedIn(isLoggedIn : Boolean)
+
+}
